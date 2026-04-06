c2a6a7c7123539c9cf4a72d77b90b39c7e758e2e
Ibrahim M Al-Shujaa <a774834218@gmail.com>
2026-04-06 16:43:39 +0300

    update

---
+++ Assets/AudioToggle.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class AudioToggle : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public TextMeshProUGUI buttonText;
+
+    private bool isMuted = true; // يبدأ صامت
+
+    void Start()
+    {
+        audioSource.mute = true;
+
+        if (buttonText != null)
+        {
+            buttonText.text = "🔊 Unmute";
+        }
+    }
+
+    public void ToggleSound()
+    {
+        isMuted = !isMuted;
+        audioSource.mute = isMuted;
+
+        if (buttonText != null)
+        {
+            if (isMuted)
+                buttonText.text = " Unmute";
+            else
+                buttonText.text = " Mute";
+        }
+    }
+}
